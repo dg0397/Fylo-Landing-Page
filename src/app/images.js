62095e38cd41664503f9f3ea0dbe94069ee69939
dg0397 <rgg0397@gmail.com>
2019-09-09 19:11:36 -0400

    first commit

---
+++ src/app/images.js
@@ -0,0 +1,13 @@
+export function importImages(){
+    require("../static/images/avatar-testimonial.jpg");
+    require("../static/images/bg-curve-desktop.svg");
+    require("../static/images/bg-curve-mobile.svg");
+    require("../static/images/favicon-32x32.png");
+    require("../static/images/icon-arrow.svg");
+    require("../static/images/icon-email.svg");
+    require("../static/images/icon-phone.svg");
+    require("../static/images/icon-quotes.svg");
+    require("../static/images/illustration-1.svg");
+    require("../static/images/illustration-2.svg");
+    require("../static/images/logo.svg");
+}
